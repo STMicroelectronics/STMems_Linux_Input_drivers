ae3ab348a0843bfa74c16b0d4181b6c6ae81ffcd
Lorenzo Bianconi <lorenzo.bianconi@st.com>
2017-02-02 09:30:07 +0100

    drivers:input:misc:st: add support to lis2dw12 acc sensor
    - i2c support
    - spi support
    
    Author: Lorenzo Bianconi <lorenzo.bianconi@st.com>
    Date: Thu Feb 2 09:30:07 2017 +0100
    Change-Id: I6da3a51b0411a2e3d1a95da7b934d60f537ac29b
    Signed-off-by: Matteo Dameno <matteo.dameno@st.com>
    Reviewed-on: https://sczcxd1104.scz.st.com/gerrit/c/linux/stm-ldd-input/+/302
    Reviewed-by: Denis CIOCCA <denis.ciocca@st.com>
    Tested-by: CI STM MSD <aosp-ger@st.com>

---
+++ drivers/input/misc/st/acc/lis2dw12/lis2dw12_core.h
@@ -0,0 +1,128 @@
+/******************** (C) COPYRIGHT 2016 STMicroelectronics ********************
+*
+* File Name          : lis2dw12_core.h
+* Authors            : AMG - Application Team
+*		     : Mario Tesi <mario.tesi@st.com>
+*		     : Giuseppe Barba <giuseppe.barba@st.com>
+*		     : Author is willing to be considered the contact and update
+*		     : point for the driver.
+* Version            : V.1.0
+* Date               : 2016/Oct/18
+* Description        : LIS2DW12 driver
+*
+********************************************************************************
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* THE PRESENT SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES
+* OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, FOR THE SOLE
+* PURPOSE TO SUPPORT YOUR APPLICATION DEVELOPMENT.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*
+********************************************************************************/
+
+#ifndef DRIVERS_INPUT_MISC_LIS2DW12_CORE_H_
+#define DRIVERS_INPUT_MISC_LIS2DW12_CORE_H_
+
+#define LIS2DW12_DEV_NAME		"lis2dw12"
+
+#define HZ_TO_PERIOD_NSEC(hz)	(1000 * 1000 * 1000 / ((u32)(hz)))
+#define MS_TO_US(x)		({ typeof(x) _x = (x); ((_x) * \
+				 ((typeof(x)) 1000));})
+#define US_TO_NS(x)		(MS_TO_US(x))
+#define MS_TO_NS(x)		(US_TO_NS(MS_TO_US(x)))
+#define US_TO_MS(x)		({ typeof(x) _x = (x); ((_x) / \
+				 ((typeof(x)) 1000));})
+#define NS_TO_US(x)		(US_TO_MS(x))
+#define NS_TO_MS(x)		(US_TO_MS(NS_TO_US(x)))
+
+enum {
+	LIS2DW12_ACCEL = 0,
+	LIS2DW12_FF,
+	LIS2DW12_TAP,
+	LIS2DW12_DOUBLE_TAP,
+	LIS2DW12_WAKEUP,
+	LIS2DW12_SENSORS_NUMB,
+};
+
+#define INPUT_EVENT_TYPE	EV_MSC
+#define INPUT_EVENT_X		MSC_SERIAL
+#define INPUT_EVENT_Y		MSC_PULSELED
+#define INPUT_EVENT_Z		MSC_GESTURE
+#define INPUT_EVENT_TIME_MSB	MSC_SCAN
+#define INPUT_EVENT_TIME_LSB	MSC_MAX
+
+#define LIS2DW12_RX_MAX_LENGTH	16
+#define LIS2DW12_TX_MAX_LENGTH	16
+
+#define to_dev(obj) container_of(obj, struct device, kobj)
+
+struct reg_rw {
+	u8 const address;
+	u8 const init_val;
+	u8 resume_val;
+};
+
+struct reg_r {
+	const u8 address;
+	const u8 init_val;
+};
+
+struct lis2dw12_transfer_buffer {
+	struct mutex buf_lock;
+	u8 rx_buf[LIS2DW12_RX_MAX_LENGTH];
+	u8 tx_buf[LIS2DW12_TX_MAX_LENGTH] ____cacheline_aligned;
+};
+
+struct lis2dw12_data;
+
+struct lis2dw12_transfer_function {
+	int (*write)(struct lis2dw12_data *cdata, u8 reg_addr,
+		     int len, u8 *data, bool b_lock);
+	int (*read)(struct lis2dw12_data *cdata, u8 reg_addr, int len, u8 *data,
+		    bool b_lock);
+};
+
+struct lis2dw12_sensor_data {
+	struct lis2dw12_data *cdata;
+	const char* name;
+	s64 timestamp;
+	u8 enabled;
+	u32 c_odr;
+	u32 c_gain;
+	u8 sindex;
+	unsigned int poll_ms;
+	struct input_dev *input_dev;
+	struct hrtimer hr_timer;
+	struct work_struct input_work;
+	ktime_t oldktime;
+};
+
+struct lis2dw12_data {
+	const char *name;
+	u8 drdy_int_pin;
+	u8 selftest_status;
+	u8 power_mode;
+	int irq;
+	s64 timestamp;
+	struct device *dev;
+	struct lis2dw12_sensor_data sensors[LIS2DW12_SENSORS_NUMB];
+	struct mutex bank_registers_lock;
+	const struct lis2dw12_transfer_function *tf;
+	struct lis2dw12_transfer_buffer tb;
+};
+
+int lis2dw12_common_probe(struct lis2dw12_data *cdata, int irq, u16 bustype);
+void lis2dw12_common_remove(struct lis2dw12_data *cdata, int irq);
+
+#ifdef CONFIG_PM
+int lis2dw12_common_suspend(struct lis2dw12_data *cdata);
+int lis2dw12_common_resume(struct lis2dw12_data *cdata);
+#endif /* CONFIG_PM */
+
+#endif /* DRIVERS_INPUT_MISC_LIS2DW12_CORE_H_ */
---
+++ drivers/input/misc/st/acc/lis2dw12/lis2dw12_spi.c
@@ -0,0 +1,209 @@
+/******************** (C) COPYRIGHT 2016 STMicroelectronics ********************
+*
+* File Name          : lis2dw12_spi.c
+* Authors            : AMG - Application Team
+*		     : Mario Tesi <mario.tesi@st.com>
+*		     : Author is willing to be considered the contact and update
+*		     : point for the driver.
+* Version            : V.1.0
+* Date               : 2016/Oct/18
+* Description        : LIS2DW12 driver
+*
+********************************************************************************
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* THE PRESENT SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES
+* OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, FOR THE SOLE
+* PURPOSE TO SUPPORT YOUR APPLICATION DEVELOPMENT.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*
+********************************************************************************/
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/spi/spi.h>
+#include <linux/input.h>
+#include <linux/types.h>
+
+#include "lis2dw12_core.h"
+
+#define SENSORS_SPI_READ	0x80
+
+static int lis2dw12_spi_read(struct lis2dw12_data *cdata, u8 reg_addr, int len,
+			   u8 *data, bool b_lock)
+{
+	int err;
+	struct spi_message msg;
+	struct spi_transfer xfers[] = {
+		{
+			.tx_buf = cdata->tb.tx_buf,
+			.bits_per_word = 8,
+			.len = 1,
+		},
+		{
+			.rx_buf = cdata->tb.rx_buf,
+			.bits_per_word = 8,
+			.len = len,
+		}
+	};
+
+	if (b_lock)
+		mutex_lock(&cdata->bank_registers_lock);
+
+	mutex_lock(&cdata->tb.buf_lock);
+	cdata->tb.tx_buf[0] = reg_addr | SENSORS_SPI_READ;
+
+	spi_message_init(&msg);
+	spi_message_add_tail(&xfers[0], &msg);
+	spi_message_add_tail(&xfers[1], &msg);
+
+	err = spi_sync(to_spi_device(cdata->dev), &msg);
+	if (err)
+		goto acc_spi_read_error;
+
+	memcpy(data, cdata->tb.rx_buf, len*sizeof(u8));
+	mutex_unlock(&cdata->tb.buf_lock);
+	if (b_lock)
+		mutex_unlock(&cdata->bank_registers_lock);
+
+	return len;
+
+acc_spi_read_error:
+	mutex_unlock(&cdata->tb.buf_lock);
+	if (b_lock)
+		mutex_unlock(&cdata->bank_registers_lock);
+
+	return err;
+}
+
+static int lis2dw12_spi_write(struct lis2dw12_data *cdata, u8 reg_addr, int len,
+			    u8 *data, bool b_lock)
+{
+	int err;
+	struct spi_message msg;
+	struct spi_transfer xfers = {
+		.tx_buf = cdata->tb.tx_buf,
+		.bits_per_word = 8,
+		.len = len + 1,
+	};
+
+	if (len >= LIS2DW12_RX_MAX_LENGTH)
+		return -ENOMEM;
+
+	if (b_lock)
+		mutex_lock(&cdata->bank_registers_lock);
+
+	mutex_lock(&cdata->tb.buf_lock);
+	cdata->tb.tx_buf[0] = reg_addr;
+
+	memcpy(&cdata->tb.tx_buf[1], data, len);
+
+	spi_message_init(&msg);
+	spi_message_add_tail(&xfers, &msg);
+	err = spi_sync(to_spi_device(cdata->dev), &msg);
+
+	mutex_unlock(&cdata->tb.buf_lock);
+	if (b_lock)
+		mutex_unlock(&cdata->bank_registers_lock);
+
+	return err;
+}
+
+
+static const struct lis2dw12_transfer_function lis2dw12_tf_spi = {
+	.write = lis2dw12_spi_write,
+	.read = lis2dw12_spi_read,
+};
+
+static int lis2dw12_spi_probe(struct spi_device *spi)
+{
+	int err;
+	struct lis2dw12_data *cdata;
+
+	cdata = devm_kzalloc(&spi->dev, sizeof(struct lis2dw12_data), GFP_KERNEL);
+	if (!cdata)
+		return -ENOMEM;
+
+	cdata->dev = &spi->dev;
+	cdata->name = spi->modalias;
+	cdata->tf = &lis2dw12_tf_spi;
+	spi_set_drvdata(spi, cdata);
+
+	err = lis2dw12_common_probe(cdata, spi->irq, BUS_SPI);
+	if (err < 0)
+		return err;
+
+	return 0;
+}
+
+static int lis2dw12_spi_remove(struct spi_device *spi)
+{
+	struct lis2dw12_data *cdata = spi_get_drvdata(spi);
+
+	lis2dw12_common_remove(cdata, spi->irq);
+
+	return 0;
+}
+
+#ifdef CONFIG_PM
+static int lis2dw12_suspend(struct device *dev)
+{
+	struct lis2dw12_data *cdata = spi_get_drvdata(to_spi_device(dev));
+
+	return lis2dw12_common_suspend(cdata);
+}
+
+static int lis2dw12_resume(struct device *dev)
+{
+	struct lis2dw12_data *cdata = spi_get_drvdata(to_spi_device(dev));
+
+	return lis2dw12_common_resume(cdata);
+}
+
+static const struct dev_pm_ops lis2dw12_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(lis2dw12_suspend, lis2dw12_resume)
+};
+
+#define LIS2DW12_PM_OPS		(&lis2dw12_pm_ops)
+#else /* CONFIG_PM */
+#define LIS2DW12_PM_OPS		NULL
+#endif /* CONFIG_PM */
+
+#ifdef CONFIG_OF
+static const struct spi_device_id lis2dw12_ids[] = {
+	{ LIS2DW12_DEV_NAME, 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(spi, lis2dw12_ids);
+
+static const struct of_device_id lis2dw12_id_table[] = {
+	{ .compatible = "st,lis2dw12", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, lis2dw12_id_table);
+#endif
+
+static struct spi_driver lis2dw12_spi_driver = {
+	.driver = {
+		.owner = THIS_MODULE,
+		.name = LIS2DW12_DEV_NAME,
+		.pm = LIS2DW12_PM_OPS,
+#ifdef CONFIG_OF
+		.of_match_table = lis2dw12_id_table,
+#endif
+	},
+	.probe    = lis2dw12_spi_probe,
+	.remove   = lis2dw12_spi_remove,
+	.id_table = lis2dw12_ids,
+};
+
+module_spi_driver(lis2dw12_spi_driver);
+
+MODULE_DESCRIPTION("STMicroelectronics lis2dw12 spi driver");
+MODULE_AUTHOR("Mario Tesi");
+MODULE_LICENSE("GPL v2");
