4ad38cc2bac40e8d7296e6f4408ea27b2dd76d39
Matteo Dameno <matteo.dameno@st.com>
2021-11-09 15:52:13 +0100

    drivers:input:misc:st: lis2ds,lsm6ds3,lsm6dsm: add missing platform_data header file
    Signed-off-by: Matteo Dameno <matteo.dameno@st.com>
    Change-Id: I26f1cf5afe2755af62bc55d5e8e4ffad7138f6bb

---
+++ include/linux/input/lis2ds.h
@@ -0,0 +1,17 @@
+/*
+ * STMicroelectronics lis2ds driver
+ *
+ * Copyright 2016 STMicroelectronics Inc.
+ *
+ * Giuseppe Barba <giuseppe.barba@st.com>
+ * V.1.1.1
+ * Licensed under the GPL-2.
+ */
+
+#ifndef __LIS2DS_PLATFORM_DATA_
+#define __LIS2DS_PLATFORM_DATA_
+struct lis2ds_platform_data {
+	int drdy_int_pin;
+};
+#endif /* __LIS2DS_PLATFORM_DATA_ */
+
---
+++ include/linux/input/lsm6dsm.h
@@ -0,0 +1,17 @@
+/*
+ * STMicroelectronics lsm6dsm driver
+ *
+ * Copyright 2014 STMicroelectronics Inc.
+ *
+ * Giuseppe Barba <giuseppe.barba@st.com>
+ * v 1.2.2
+ * Licensed under the GPL-2.
+ */
+
+#ifndef __LSM6DSM_PLATFORM_DATA_
+#define __LSM6DSM_PLATFORM_DATA_
+struct lsm6dsm_platform_data {
+	int drdy_int_pin;
+};
+#endif /* __LSM6DSM_PLATFORM_DATA_ */
+
