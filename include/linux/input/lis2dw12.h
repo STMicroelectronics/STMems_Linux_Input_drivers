680f4571756327e7bde69fd96d7fa6db51c410dd
Matteo Dameno <matteo.dameno@st.com>
2021-11-09 15:52:13 +0100

    drivers:input:misc:st: lis2w12: fix build when CONFIG_OF is not set
    add missing platform_data header file
    
    Date: Tue Nov 9 15:52:13 2021 +0100
    Change-Id: I100ee8ae2508a5fb5b2a838e0797deeebf0262d8
    Signed-off-by: Matteo Dameno <matteo.dameno@st.com>

---
+++ include/linux/input/lis2dw12.h
@@ -0,0 +1,17 @@
+/*
+ * STMicroelectronics lis2dw12 driver
+ *
+ * Copyright 2016 STMicroelectronics Inc.
+ *
+ * Giuseppe Barba <giuseppe.barba@st.com>
+ *  V.1.0
+ * Licensed under the GPL-2.
+ */
+
+#ifndef __LIS2DW12_PLATFORM_DATA_
+#define __LIS2DW12_PLATFORM_DATA_
+struct lis2dw12_platform_data {
+	int drdy_int_pin;
+};
+#endif /* __LIS2DW12_PLATFORM_DATA_ */
+
